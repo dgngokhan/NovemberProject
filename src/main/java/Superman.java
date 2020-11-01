77e6859c2f4f30a54ec8b14e26c7a3a9d7e2a339
dgngokhan <67300102+dgngokhan@users.noreply.github.com>
2020-11-01 09:33:35 -0700

    great change2

--- src/main/java/Superman.java
+++ src/main/java/Superman.java
@@ -1,10 +1,9 @@
 public class Superman {
     public static void main(String[] args) {
         System.out.println("Company Name: Uber");
-        System.out.println("Employee Name: George");
-        System.out.println("Employee ID: 12345");
+        System.out.println("Employee Name: John");
+        System.out.println("Employee ID: 19857");
         System.out.println("Job Title: Driver");
-        System.out.println("Salary: $100000");
         System.out.println("Salary: $190000");
     }
 }
