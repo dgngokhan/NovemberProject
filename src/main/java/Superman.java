cce0036c93955a620efe29b7aaaed5cc4f82975a
dgngokhan <67300102+dgngokhan@users.noreply.github.com>
2020-11-01 08:49:27 -0700

    initial commit

--- src/main/java/Superman.java
+++ src/main/java/Superman.java
@@ -1,6 +1,9 @@
 public class Superman {
     public static void main(String[] args) {
-        System.out.println("Superman");
-
+        System.out.println("Company Name: Uber");
+        System.out.println("Employee Name: George");
+        System.out.println("Employee ID: 12345");
+        System.out.println("Job Title: Driver");
+        System.out.println("Salary: $100000");
     }
 }
